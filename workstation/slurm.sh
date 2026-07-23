6fd26b5819ec0c254958850ab8637f19ff8defe6
Wladyslaw Zawadzki <wladek@newton.mpipks-dresden.mpg.de>
2026-07-23 09:46:34 +0200

    Bigger PD, alternative PD

--- workstation/slurm.sh
+++ workstation/slurm.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#SBATCH --partition medium
-#SBATCH --mem-per-cpu 16G
-#SBATCH --time 12:00:00
+#SBATCH --partition long
+#SBATCH --mem-per-cpu 64G
+#SBATCH --time 48:00:00
 #SBATCH --job-name ABP
 #SBATCH --error error-%j.txt
 #
 #######################################
 
 source .venv/bin/activate
-python abp_simulation.py --PD -f pd_G1.txt -l1 0.25 -u1 4 -l2 0.125 -u2 2 -n 16 -G 1 -N 1000
+python abp_simulation.py --PD -f pdbig_G1.txt -l1 0.25 -u1 8 -l2 0.125 -u2 4 -n 32 -G 1 -N 1000
---
+++ workstation/slurm4.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH --partition medium
+#SBATCH --mem-per-cpu 16G
+#SBATCH --time 12:00:00
+#SBATCH --job-name ABP
+#SBATCH --error error-%j.txt
+#
+#######################################
+
+source .venv/bin/activate
+python abp_simulation.py --PDA -f pdalt_G1.txt -l1 0.25 -u1 4 -l2 0.5 -u2 8 -n 16 -G 1 -N 1000
