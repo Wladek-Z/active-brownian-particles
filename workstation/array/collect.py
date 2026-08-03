832be0e59a81ddd70b352017656a3a4a761465ab
Wladyslaw Zawadzki <wladek@newton.mpipks-dresden.mpg.de>
2026-08-03 11:42:06 +0200

    Calculated individual mean velocities

--- workstation/array/collect.py
+++ workstation/array/collect.py
@@ -2,23 +2,24 @@ import numpy as np
 from pathlib import Path
 import argparse
 
-def collect_mean_vx(folder, Ps, Pf, tc, dt):
+def collect_mean_vx(input, output, Ps, timechain, dt):
     """
     Calculate the mean instantaneous longitudinal velocity for a given combination of
     Peclet numbers by taking the velocity at each pair of consecutive timesteps, separated 
     by logscale blocks. Save result to file.
     
     Arguments:
-        folder: directory containing the logscaled trajectories for each (Ps, Pf)
+        input: directory containing raw trajectories
+        output: file to store results
         Ps: swim Peclet number
-        tc: logscale timechain
+        timechain: logscale timechain file
         dt: simulation timestep
 
     Returns:
         mean longitudinal velocity
     """
-    # Get filepath to directory containing data regarding specified Ps, Pf
-    subfolder = f"{folder}/{Ps} {Pf}"
+    # Read in timechain from file
+    tc = np.loadtxt(timechain, dtype=np.int64)
     # Find the second measurement of each logscale block (+1 to skip header)
     measurements = np.arange(1, len(tc)-1)[np.diff((tc[1:] - tc[:-1]) == 1)][::2] + 1
     # Initialise sum of x-velocities, particle counter
@@ -26,7 +27,7 @@ def collect_mean_vx(folder, Ps, Pf, tc, dt):
     particles = 0
 
     # Iterate over each particle trajectory in directory
-    for file in Path(subfolder).glob("*.txt"):
+    for file in Path(input).glob("*.txt"):
         # Increment number of particles
         particles += 1
         with open(file, "r") as f:
@@ -45,10 +46,9 @@ def collect_mean_vx(folder, Ps, Pf, tc, dt):
     # Calculate mean velocity in terms of Ps
     mean_vx = sum_vx / len(measurements) / particles / Ps   
 
-    # Save result to file
-    output = f"{folder} results/mean velocities/{Ps} {Pf}.txt"     
+    # Save result to file     
     with open(output, "w") as f:
-        f.write(f"{Ps} {Pf} {mean_vx}")
+        f.write(f"{mean_vx}")
 
 def get_mean_vx2(folder, Ps, tc, dt):
     """
@@ -84,12 +84,12 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-F', type=str, default=None, help='Directory containing data for each phase point')
     parser.add_argument('--VX', action='store_true', help='Collect mean velocity data for a phase point')
-    parser.add_argument('-Ps', type=str, help='Filepath to swim Peclet number parameter file')
-    parser.add_argument('-Pf', type=str, help='Filepath to flow Peclet number parameter file')
+    parser.add_argument('-Ps', type=float, help='Filepath to swim Peclet number parameter file')
+    parser.add_argument('-Pf', type=float, help='Filepath to flow Peclet number parameter file')
     parser.add_argument('-tc', type=str, help='Filepath to the logscale timechain file')
     parser.add_argument('-dt', type=float, default=0.001, help='Simulation timestep')
     parser.add_argument('-o', type=str, help='Name of output file')
     args = parser.parse_args()
 
     if args.VX:
-        collect_mean_vx(args.F, args.Ps, args.Pf, args.tc, args.dt)
+        collect_mean_vx(args.F, args.o, args.Ps, args.tc, args.dt)
