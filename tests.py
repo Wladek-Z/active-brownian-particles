d180b398af734d11711cb31c961c05a3b1a85357
Wladek Z <vladek.zawadzki@gmail.com>
2026-08-04 13:20:08 +0200

    sync

--- tests.py
+++ tests.py
@@ -1,8 +1,20 @@
 import numpy as np
-import time
 
-filename = "workstation/array/G 1 results/mean velocities/1.25 1.0.txt"
+filename = "workstation/array/G 1 results/MSD ind/4.00 7.0 n20.npz"
 
-vx = np.loadtxt(filename, dtype=float)
+data = np.load(filename)
+t = data['time']
+msds = data['MSD']
 
-print(vx)
+
+array = np.empty([len(t), len(msds) + 1])
+array[:, 0] = t
+string = "Time "
+
+for i in range(1, len(msds) + 1):
+    string += f"MSD{i} "
+    array[:, i] = msds[i - 1]
+
+
+
+np.savetxt("test_4.00_7.0_n20.txt", array, header=string, fmt='%.3f')
