f62e73c446752254113f91f2416c051f0a3d407a
Wladyslaw Zawadzki <wladek@newton.mpipks-dresden.mpg.de>
2026-08-05 16:39:02 +0200

    instantaneous velocity pdf for 4.25 7.5

--- workstation/array/slurm collect1.sh
+++ workstation/array/slurm collect1.sh
@@ -12,7 +12,10 @@ G=1
 x=$1
 y=$2
 
+output="G ${G} results/velocities"
+mkdir -p "${output}"
+
 echo "Task $SLURM_JOB_ID: Ps=$x Pf=$y"
 
 source ../.venv/bin/activate
-python collect.py -i "G ${G}/${x} ${y}" -Ps $x --trajectory -o "trajs ${x} ${y}.npz" -Pf $y -s 20
+python collect.py -i "G ${G}/${x} ${y}" -Ps $x --velocities -o "${output}/v_hist ${x} ${y}.txt"
