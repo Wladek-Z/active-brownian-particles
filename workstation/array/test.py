d7a6f94c5b974a117321c6075f8f2807343f5171
Wladek Z <vladek.zawadzki@gmail.com>
2026-08-07 11:14:00 +0200

    sync

--- workstation/array/test.py
+++ workstation/array/test.py
@@ -2,17 +2,10 @@ from pathlib import Path
 import numpy as np
 import os
 
-path1 = 'G 1 results/MSD o0/0.25 0.5.txt'
 
-with open(path1, 'rb') as f:
-    try:  # catch OSError in case of a one line file 
-        f.seek(-2, os.SEEK_END)
-        while f.read(1) != b'\n':
-            f.seek(-2, os.SEEK_CUR)
-    except OSError:
-        f.seek(0)
-    last_line = f.readline().decode()
+filename = 'tester.txt'
 
-x, y = last_line.split(' ')
+open(filename, 'w')
 
-print(f"{x} {y}")
+with open(filename, 'a') as f:
+    f.write("goodbye")
