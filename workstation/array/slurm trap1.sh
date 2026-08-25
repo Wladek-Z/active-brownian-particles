c5262dae7aad6fc988d4d24b73b01caaf28d2f3d
Wladyslaw Zawadzki <wladek@newton.mpipks-dresden.mpg.de>
2026-08-25 16:50:05 +0200

    ttds btds with alternative boundary percentages

--- workstation/array/slurm trap1.sh
+++ workstation/array/slurm trap1.sh
@@ -15,4 +15,4 @@ y=$2
 echo "Task $SLURM_JOB_ID: Ps=$x Pf=$y"
 
 source ../.venv/bin/activate
-python abp_trap.py -Ps $x -Pf $y -G $G -ttd "G ${G} results/ttd3 ${x} ${y}.txt" -btd "G ${G} results/btd3 ${x} ${y}.txt" -c "/data/biophys/ABP_channel/G ${G}/${x} ${y}"
+python abp_trap.py -Ps $x -Pf $y -G $G -ttd "G ${G} results/ttd3 ${x} ${y}.txt" -btd "G ${G} results/btd3 ${x} ${y}.txt" -c "/data/biophys/ABP_channel/G ${G}/${x} ${y}" -tdx "G ${G} results/tdx3 ${x} ${y}.txt" -bdx "G ${G} results/bdx3 ${x} ${y}.txt"
